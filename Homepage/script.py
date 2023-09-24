36da7cd6ef41e05dd16f26c0575868d9ae8c621d
riteshsiva <ritesh.sivanathan@gmail.com>
2023-09-24 17:22:31 -0400

    deleted all files (migrating to Flask temp)

--- Homepage/script.py
+++ Homepage/script.py
@@ -2,12 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def index():
     return render_template('base.html')
 
-
-app.run(debug=True)
-
+if __name__ == '__main__':
+    app.run(debug=True)
