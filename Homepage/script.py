b625dd44103275b7553479f1a9f66c66aef93091
riteshsiva <ritesh.sivanathan@gmail.com>
2023-09-24 20:38:17 -0400

    added articles, fixed tons of bugs and add sum stf

--- Homepage/script.py
+++ Homepage/script.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('homepage.html')
+
+@app.route('/base.html')
+def readmore():
     return render_template('base.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
