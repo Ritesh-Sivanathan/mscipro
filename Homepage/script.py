e6874745ec0479a11ad478a1af480e5571c20ed1
riteshsiva <ritesh.sivanathan@gmail.com>
2023-10-22 14:46:57 -0400

    +cleanup of lots of the documents and struct -docs

--- Homepage/script.py
+++ Homepage/script.py
@@ -21,11 +21,11 @@ def readmore():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('Login & Register/login.html')
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('Login & Register/register.html')
 
 @app.route('/contact')
 def contact():
@@ -33,56 +33,15 @@ def contact():
 
 @app.route('/mathematics')
 def math():
-    return render_template('Subjects/mathematics.html')
+    return render_template('Subjects/math/mathematics.html')
 
 @app.route('/science')
 def science():
-    return render_template('Subjects/science.html')
+    return render_template('Subjects/science/science.html')
 
 @app.route('/programming')
 def programming():
-    return render_template('Subjects/programming.html')
-
-@app.route('/auth', methods=['POST'])
-def authenticate():
-    if request.referrer.endswith('/login'):
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        # Replace this with your actual authentication logic.
-        if username in sample_user and sample_user[username] == password:
-            session['logged_in'] = True
-            return redirect(url_for('dashboard'))
-        else:
-           return mail_list
-    
-    elif request.referrer.endswith('/register'):
-        email = request.form.get('email')
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        with open('Homepage/users.txt', 'r+') as file:
-            if username not in file.read():
-                file.write(f"{username} : {password}")
-            else:
-                return render_template('register.html')
-
-    return render_template('homepage.html')
-
-        
-@app.route('/dashboard')
-def dashboard():
-    if session.get('logged_in'):
-        return render_template('homepage.html')
-    else:
-        return redirect(url_for('login'))
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    return redirect(url_for('login'))
-
-
+    return render_template('Subjects/programming/programming.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
