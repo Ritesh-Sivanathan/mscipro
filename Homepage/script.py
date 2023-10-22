253d496e78bee68bf1774b40dbd1528392d1f10d
riteshsiva <ritesh.sivanathan@gmail.com>
2023-10-22 17:15:19 -0400

    +minor changes?

--- Homepage/script.py
+++ Homepage/script.py
@@ -4,12 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
-app.secret_key = 'your_secret_key'
 
-sample_user = {
-    'username': 'your_username',
-    'password': 'your_password',
-}
 
 @app.route('/')
 def index():
