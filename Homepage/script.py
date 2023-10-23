2bb564534ce5ae06024ea290ad237adcca2b3a94
Ritesh Sivanathan <82885975+Ritesh-Sivanathan@users.noreply.github.com>
2023-10-23 17:44:11 +0000

    +all grade practices

--- Homepage/script.py
+++ Homepage/script.py
@@ -4,8 +4,6 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def index():
     return render_template('homepage.html')
@@ -44,6 +42,39 @@ def programming():
 @app.route('/gr1practice')
 def gr1prac():
     return render_template('Subjects/math/Grade 1 PP/grade1practice.html')
+
+@app.route('/gr2practice')
+def gr2pract():
+    return render_template('Subjects/math/Grade 2 PP/grade2practice.html')
+
+@app.route('/gr3practice')
+def gr3pract():
+    return render_template('Subjects/math/Grade 3 PP/grade2practice.html')
+
+@app.route('/gr4practice')
+def gr4pract():
+    return render_template('Subjects/math/Grade 4 PP/grade2practice.html')
+
+@app.route('/gr5practice')
+def gr5pract():
+    return render_template('Subjects/math/Grade 5 PP/grade2practice.html')
+
+@app.route('/gr6practice')
+def gr6pract():
+    return render_template('Subjects/math/Grade 6 PP/grade2practice.html')
+
+@app.route('/gr7practice')
+def gr7pract():
+    return render_template('Subjects/math/Grade 7 PP/grade2practice.html')
+
+@app.route('/gr8practice')
+def gr8pract():
+    return render_template('Subjects/math/Grade 8 PP/grade2practice.html')
+
+@app.route('/gr9practice')
+def gr9pract():
+    return render_template('Subjects/math/Grade 9 PP/grade2practice.html')
+
 
 
 if __name__ == '__main__':
