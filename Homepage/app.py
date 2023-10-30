611c93e112abb7a2056476d562ba3e418a9a7818
riteshsiva <ritesh.sivanathan@gmail.com>
2023-10-29 23:17:57 -0400

    +added visual changes +db for flask (NOTWORKING)

--- Homepage/app.py
+++ Homepage/app.py
@@ -1,8 +1,23 @@
 # https://www.geeksforgeeks.org/login-and-registration-project-using-flask-and-mysql/
 
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+
+migrate = Migrate(app, db)  # 'db' should be your SQLAlchemy database instance
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable tracking modifications
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 @app.route('/')
 def index():
@@ -34,6 +49,10 @@ def science():
 
 @app.route('/programming')
 def programming():
+    return render_template('Subjects/programming/programming.html')
+
+@app.route('/loading')
+def loading():
     return render_template('Subjects/programming/programming.html')
 
 
