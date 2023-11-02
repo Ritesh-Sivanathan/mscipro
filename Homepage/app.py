a06a2954b6e2bb0ec52f8c5047ec970864cf6333
Ritesh Sivanathan <82885975+Ritesh-Sivanathan@users.noreply.github.com>
2023-11-02 17:07:39 +0000

    +i dont know

--- Homepage/app.py
+++ Homepage/app.py
@@ -10,7 +10,6 @@ from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
 db = SQLAlchemy(app)
