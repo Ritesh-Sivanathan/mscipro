f3d93ea1e9508008c1ed4df1df6bd7d070dc965a
riteshsiva <ritesh.sivanathan@gmail.com>
2023-11-07 22:52:43 -0500

    +notifier homepage +overflow-x issue fixed all subjs

--- Homepage/static/Main Pages Styling/homepagescript.js
+++ Homepage/static/Main Pages Styling/homepagescript.js
@@ -1,1 +1,11 @@
+<script>
 
+const hidebutton = document.getElementById("hide");
+const scrollingtext = document.getElementById("notifier");
+
+hidebutton.addEventListener("click", function() {
+    hidebutton.style.display = "none";
+    scrollingtext.style.display = "none";
+});
+
+</script>
