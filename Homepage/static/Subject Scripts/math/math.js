c6351bd958f58c96fd4025b4542af39e528d658e
Ritesh Sivanathan <82885975+Ritesh-Sivanathan@users.noreply.github.com>
2023-12-09 17:20:43 +0000

    +

--- Homepage/static/Subject Scripts/math/math.js
+++ Homepage/static/Subject Scripts/math/math.js
@@ -7,6 +7,9 @@ window.onload = function() { // when site is loaded ...
     var span = document.getElementsByClassName("close")[0];
     var sat = document.getElementById('open-modal');
     var usercl = document.documentElement.style.setProperty("--main-background-color", "green");
+    var cookies = document.cookies;
+
+    console.log(cookies);
 
     sat.addEventListener("click", function() { // add event listener for SAT
         modal.style.display = "block";
@@ -17,5 +20,6 @@ window.onload = function() { // when site is loaded ...
         modal.style.display = "none";
     });
 
+    
 
 }
