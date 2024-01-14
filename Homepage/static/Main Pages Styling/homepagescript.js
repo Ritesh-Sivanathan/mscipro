9f6ae0c2271e0eda2aac442aab3c95c7f333bd8e
RITESH SIVANATHAN <ritesh.sivanathan@gmail.com>
2024-01-13 23:07:29 -0500

    revamping homepage in prog

--- Homepage/static/Main Pages Styling/homepagescript.js
+++ Homepage/static/Main Pages Styling/homepagescript.js
@@ -1,12 +1,12 @@
 window.onload = function() {
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById("myModal");
     var span = document.getElementsByClassName("close")[0];
 
     if (localStorage.getItem('modalShown') !== 'true') {
         modal.style.display = "block";
         localStorage.setItem('modalShown', 'true');
     }
-
+    
     span.addEventListener("click", function() {
         modal.style.display = "none";
     });
