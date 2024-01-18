b7f71c760f616fee93e00fef991e34b72ab08b22
RITESH SIVANATHAN <ritesh.sivanathan@gmail.com>
2024-01-17 23:22:03 -0500

    styled math,science and programming. prototyped a div hover animation for the math page

--- Homepage/static/Subject Scripts/math/math.js
+++ Homepage/static/Subject Scripts/math/math.js
@@ -1,25 +1,46 @@
-window.onload = function() { // when site is loaded ...
-
-    // selecting respective variables from page
-    // including SAT modal, close modal btn, SAT open btn
-
+window.onload = function() {
     var modal = document.getElementById('myModal');
     var span = document.getElementsByClassName("close")[0];
     var sat = document.getElementById('open-modal');
-    var usercl = document.documentElement.style.setProperty("--main-background-color", "green");
-    var cookies = document.cookies;
 
-    console.log(cookies);
+    var gradeWork = document.getElementById('ofgrwork');
 
-    sat.addEventListener("click", function() { // add event listener for SAT
+    var allBoxes = [
+        document.getElementById('exams'),
+        document.getElementById('ourresources'),
+        document.getElementById('rsrcs')
+    ];
+
+    sat.addEventListener("click", function() {
         modal.style.display = "block";
-    })
-    
-    
-    span.addEventListener("click", function() { // add event listener for close btn
+    });
+
+    span.addEventListener("click", function() {
         modal.style.display = "none";
     });
 
-    
+    if (localStorage.getItem('mathRUpdate') == null) {
+        alert("All resources on this page have been filled as of 1/17/2024. Thank you for your patience. I will be working on a feature to easily manage links so I can fill them out quicker in the future.")
+        localStorage.setItem('mathRUpdate', 0)
+    }
 
 }
+//     gradeWork.addEventListener('mouseenter', function() {
+//         for (var i = 0; i < allBoxes.length; i++) {
+//             allBoxes[i].style.animation = "none";
+//             void allBoxes[i].offsetWidth; 
+//             allBoxes[i].style.animation = "boxHoverFade 0.5s linear";
+//             allBoxes[i].style.opacity = 0;
+//         }
+//     });
+    
+//     gradeWork.addEventListener('mouseleave', function() {
+//         for (var i = 0; i < allBoxes.length; i++) {
+//             allBoxes[i].style.animation = "none"; 
+//             void allBoxes[i].offsetWidth; 
+//             allBoxes[i].style.animation = "boxHoverFadeRelease 0.5s linear";
+//             gradeWork.style.animation = "none";
+//             allBoxes[i].style.opacity = 1;
+//         }
+//     });
+// }    
